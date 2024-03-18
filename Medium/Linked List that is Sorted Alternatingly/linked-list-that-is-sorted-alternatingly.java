fc5abcee781bb88a63985cb0fb9c588e0d90d265
Aditya Babu Prajapati <108863108+adityaprajapati10@users.noreply.github.com>
2024-03-18 18:39:05 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Linked List that is Sorted Alternatingly/linked-list-that-is-sorted-alternatingly.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class GFG {
+    static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	public static void main (String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    
+		    Node head = new Node(sc.nextInt());
+		    Node tail = head;
+		    
+		    for(int i=1; i<n; i++){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		    Solution obj = new Solution();
+		    head = obj.sort(head);
+		    printList(head);
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+
+/*
+class Node {
+    int data;
+    Node next;
+    
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    
+   public Node sort(Node head){
+        //your code here, return the head of the sorted list
+        int n = 0;
+        Node current = head ;
+        while(current != null){
+            n++;
+            current = current.next;
+        }
+        int[] arr = new int[n];
+        int index = 0;
+        current = head;
+        while(current != null){
+            arr[index] = current.data;
+            index++;
+            current = current.next;
+        }
+        index = 0;
+        Arrays.sort(arr);
+        current = head;
+        while(current != null){
+            current.data = arr[index] ;
+            index++;
+            current = current.next;
+        }
+        return head;
+   }
+
+
+}
