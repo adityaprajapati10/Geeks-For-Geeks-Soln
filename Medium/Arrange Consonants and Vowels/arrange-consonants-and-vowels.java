a1f021c3e3fbbf47e7656def6f307e95b18c4810
Aditya Babu Prajapati <108863108+adityaprajapati10@users.noreply.github.com>
2024-05-01 10:57:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Arrange Consonants and Vowels/arrange-consonants-and-vowels.java
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    char data;
+    Node next;
+    
+    public Node(char data){
+        this.data = data;
+        next = null;
+    }
+}
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    Node head = null, tail = null;
+		    
+		    char head_c = sc.next().charAt(0);
+		    head = new Node(head_c);
+		    tail = head;
+		    
+		    while(n-- > 1){
+		        tail.next = new Node(sc.next().charAt(0));
+		        tail = tail.next;
+		    }
+		    
+		    Solution obj = new Solution();
+		    //show(head);
+		    show(obj.arrangeCV(head));
+		    
+		}
+	}
+	
+	public static void po(Object o){
+	    System.out.println(o);
+	}
+	
+	public static void show(Node head){
+        while(head != null){
+            System.out.print(head.data+" ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+Structure of node class is:
+class Node {
+    char data;
+    Node next;
+    
+    public Node(char data){
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution {
+    
+    public Node arrangeCV(Node head){
+        //write code here and return the head of changed linked list
+        Node dummyV = new Node('v');
+        Node dummyC = new Node('c');
+        Node tempV = dummyV;
+        Node tempC = dummyC;
+        while(head != null){
+            if(isVowel(head.data)){
+                tempV.next = head;
+                tempV = tempV.next;
+            }else{
+                tempC.next = head;
+                tempC = tempC.next;
+            }
+            head = head.next;
+        }
+        tempV.next = dummyC.next;
+        tempC.next = null;
+        return dummyV.next;
+    }
+    public boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
