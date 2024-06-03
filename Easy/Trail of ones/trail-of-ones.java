8f59880b35970f1026e9dd69ba9b757f7a0dfa34
Aditya Babu Prajapati <108863108+adityaprajapati10@users.noreply.github.com>
2024-06-03 19:54:03 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Trail of ones/trail-of-ones.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.numberOfConsecutiveOnes(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int numberOfConsecutiveOnes(int n) {
+        // code here
+        if (n == 2)
+        {
+            return 1;
+        }
+
+        int mod = 1000000007;
+        int zeroOnes = 1;
+        int onlyOnes = 2;
+        int power = 2;
+
+        for (int i = 3; i < n; i++)
+        {
+            int temp = (zeroOnes + onlyOnes) % mod;
+            power = (power * 2) % mod;
+            onlyOnes = (power + zeroOnes) % mod;
+            zeroOnes = temp;
+        }
+
+        return (onlyOnes + zeroOnes) % mod;
+    }
+}
