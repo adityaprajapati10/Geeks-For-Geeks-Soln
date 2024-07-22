bd39862bb097ca3264b2b62ab8761a4740ce0af0
Aditya Babu Prajapati <108863108+adityaprajapati10@users.noreply.github.com>
2024-07-22 18:17:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int num = sc.nextInt();
+            int k = sc.nextInt();
+            
+            int arr[] = new int[num];
+            for(int i = 0; i < num; i++)
+                arr[i] = sc.nextInt();
+            
+            ArrayList <Integer> res = new Solution().nearlySorted(arr, num, k);
+            for (int i = 0; i < res.size (); i++)
+                System.out.print (res.get (i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return the sorted array.
+    ArrayList<Integer> nearlySorted(int arr[], int num, int k) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        ArrayList<Integer> nums = new ArrayList<>();
+        for (int i : arr) {
+            nums.add(i);
+        }
+        return nums;
+    }
+}
