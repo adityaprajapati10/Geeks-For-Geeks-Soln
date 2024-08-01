233e2e5a2cfa91e0ca512f799a4ce78b06b46409
Aditya Babu Prajapati <108863108+adityaprajapati10@users.noreply.github.com>
2024-08-01 06:26:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.java
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.java
@@ -35,38 +35,36 @@ class Solution {
         // Your code here
         int n = mat.length;
         ArrayList<String> ans = new ArrayList<>();
-        int visited[][] = new int[n][n];
-        for(int[] row : visited){
-            Arrays.fill(row, 0);
+        if (mat[0][0] == 1) {
+            StringBuilder temp = new StringBuilder();
+            solve(0, 0, n, ans, temp, mat);
         }
-        if(mat[0][0] == 1) solve(0, 0, n, ans, "", visited, mat);
         return ans;
     }
-    
-    void solve(int i, int j, int n, ArrayList<String> ans, String move, int visited[][], int[][] mat){
-        if(i == n-1 && j == n-1){
-            ans.add(move);
+    static void solve(int i , int j , int n, ArrayList<String> ans , StringBuilder temp, int[][] mat){
+        if(!(i >= 0 && i < n && j >= 0 && j < n )|| mat[i][j] == 0 )
             return;
+        if(i == n-1 && j == n-1){
+            ans.add(temp.toString());
         }
-        if(i+1 < n && visited[i+1][j] != 1 && mat[i+1][j] == 1){
-            visited[i][j] = 1;
-            solve(i+1, j, n, ans, move+"D", visited, mat);
-            visited[i][j] = 0;
-        }
-        if(j-1 >=0 && visited[i][j-1] != 1 && mat[i][j-1] == 1){
-            visited[i][j] = 1;
-            solve(i, j-1, n, ans, move+"L", visited, mat);
-            visited[i][j] = 0;
-        }
-        if(j+1 < n && visited[i][j+1] != 1 && mat[i][j+1] == 1){
-            visited[i][j] = 1;
-            solve(i, j+1, n, ans, move+"R", visited, mat);
-            visited[i][j] = 0;
-        }
-        if(i-1 >=0 && visited[i-1][j] != 1 && mat[i-1][j] == 1){
-            visited[i][j] = 1;
-            solve(i-1, j, n, ans, move+"U", visited, mat);
-            visited[i][j] = 0;
-        }
+        mat[i][j] = 0;
+
+        temp.append('D');
+        solve(i + 1, j, n, ans, temp, mat);
+        temp.deleteCharAt(temp.length() - 1);
+
+        temp.append('R');
+        solve(i, j + 1, n, ans, temp, mat);
+        temp.deleteCharAt(temp.length() - 1);
+
+        temp.append('U');
+        solve(i - 1, j, n, ans, temp, mat);
+        temp.deleteCharAt(temp.length() - 1);
+
+        temp.append('L');
+        solve(i, j - 1, n, ans, temp, mat);
+        temp.deleteCharAt(temp.length() - 1);
+
+        mat[i][j] = 1;
     }
 }
