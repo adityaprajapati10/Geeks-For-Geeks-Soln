d2b5d911cfa7bedc67be6d4405779b81c491cf30
Aditya Babu Prajapati <108863108+adityaprajapati10@users.noreply.github.com>
2024-08-17 13:29:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            long[] ans = new long[n];
+            ans = ob.productExceptSelf(array);
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static long[] productExceptSelf(int nums[]) {
+        // code here
+        long [] arr = new long[nums.length];
+        int idx = 0;
+        for(int i = 0 ; i<nums.length ; i++){
+            long mul = 1;
+            for(int j = 0 ; j<nums.length ; j++){
+                if(i != j){
+                    mul *= nums[j];
+                }
+            }
+            arr[idx++] = mul;
+        }
+        return arr;
+    }
+}
