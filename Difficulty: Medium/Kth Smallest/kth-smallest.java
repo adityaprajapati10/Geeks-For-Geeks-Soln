ad2f52f64f90b7f3c35a9ca1bd955eee631ecc4f
Aditya Babu Prajapati <108863108+adityaprajapati10@users.noreply.github.com>
2024-08-19 23:34:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.java
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -1,74 +1,56 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 /*package whatever //do not write package name here */
 
 import java.io.*;
 import java.util.*;
+
 class GFG {
-	public static void main (String[] args) {
-		Scanner sc=new Scanner(System.in);
-		PrintWriter out = new PrintWriter(System.out);
-		int t=sc.nextInt();
-		
-		while(t-->0)
-		{
-		    int n=sc.nextInt();
-		    
-		    int arr[]=new int[n];
-		    
-		    for(int i=0;i<n;i++)
-		    arr[i]=sc.nextInt();
-		    
-		    int k=sc.nextInt();
-		    Solution ob = new Solution();
-		    out.println(ob.kthSmallest(arr, 0, n-1, k));
-		}
-		out.flush();
-	}
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int key = Integer.parseInt(in.readLine().trim());
+            Solution ob = new Solution();
+            out.println(ob.kthSmallest(arr, key));
+        }
+        out.flush();
+    }
 }
 
 // } Driver Code Ends
 
 
-//User function Template for Java
 
-class Solution{
-    public static int kthSmallest(int[] arr, int l, int r, int k) 
-    { 
-        //Your code here
-        return quickSort(arr , l , r , k);
-    } 
-    public static int quickSort(int[] arr , int l , int h ,int k){
-        if(l<=h){
-            int mid = partition(arr ,  l ,  h);
-            if(mid == k-1){
-                return arr[mid];
-            }
-            else if(mid < k-1){
-                return quickSort(arr , mid+1 , h , k);
-            }
-            else{
-                return quickSort(arr , l , mid-1 , k);
-            }
-        }
-        return -1;
-    }
-    public static int partition(int[] arr , int l ,int h){
-        int pivot = arr[l];
-        int i = l;
-        for(int j = i+1 ; j<= h ; j++){
-            if(arr[j] < pivot){
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        int temp = arr[l];
-        arr[l] = arr[i];
-        arr[i] = temp;
-        return i;
-    }
 
+// User function Template for Java
+
+class Solution {
+    public static int kthSmallest(int[] arr, int k) {
+        // Your code here
+PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int val : arr){
+            pq.add(val);
+            if(pq.size() > k) pq.poll();
+        }
+        return pq.poll();
+    }
 }
